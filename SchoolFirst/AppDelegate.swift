782cd73e09355a5f830697e8b1896382e427b25f
musharaf1912 <musharaf@lifeboat.co.in>
2025-10-27 16:00:35 +0530

    Prod firebase

--- SchoolFirst/AppDelegate.swift
+++ SchoolFirst/AppDelegate.swift
@@ -17,9 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
 
-         FirebaseApp.configure()
+        configureFirebase()
 
-         if #available(iOS 15.0, *) {
+        if #available(iOS 15.0, *) {
             let navBarAppearance = UINavigationBarAppearance()
             navBarAppearance.configureWithOpaqueBackground()
             navBarAppearance.backgroundColor = .white
@@ -31,21 +31,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             UINavigationBar.appearance().compactAppearance = navBarAppearance
         }
 
-         IQKeyboardManager.shared.isEnabled = true
+        IQKeyboardManager.shared.isEnabled = true
         IQKeyboardManager.shared.enableAutoToolbar = true
         IQKeyboardManager.shared.resignOnTouchOutside = true
+
+        Analytics.logEvent("app_launched", parameters: nil)
+        Crashlytics.crashlytics().log("App started")
 
         return true
     }
 
-     func application(_ application: UIApplication,
+    private func configureFirebase() {
+         #if PROD
+        let fileName = "GoogleService-Info-Prod"
+        #else
+        let fileName = "GoogleService-Info-dev"
+        #endif
+
+        guard let filePath = Bundle.main.path(forResource: fileName, ofType: "plist"),
+              let options = FirebaseOptions(contentsOfFile: filePath) else {
+            print("Could not find \(fileName).plist. Make sure it's added to your target and the name matches exactly.")
+            return
+        }
+
+        FirebaseApp.configure(options: options)
+        print("Firebase configured with \(fileName).plist")
+    }
+
+    func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication,
-                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-     }
+                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
 }
 
