0f63e76718a28f6bf2f7f73b224302a67f143182
musharaf1912 <musharaf@lifeboat.co.in>
2025-10-27 18:41:16 +0530

    Push notifications

--- SchoolFirst/AppDelegate.swift
+++ SchoolFirst/AppDelegate.swift
@@ -8,9 +8,11 @@
 import UIKit
 import IQKeyboardManagerSwift
 import Firebase
+import UserNotifications
+import FirebaseMessaging
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
 
     func application(
         _ application: UIApplication,
@@ -18,22 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     ) -> Bool {
 
         configureFirebase()
+        setupNavigationBar()
+        setupKeyboardManager()
 
-        if #available(iOS 15.0, *) {
-            let navBarAppearance = UINavigationBarAppearance()
-            navBarAppearance.configureWithOpaqueBackground()
-            navBarAppearance.backgroundColor = .white
-            navBarAppearance.shadowColor = .lightGray
-            navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.black]
-
-            UINavigationBar.appearance().standardAppearance = navBarAppearance
-            UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
-            UINavigationBar.appearance().compactAppearance = navBarAppearance
-        }
-
-        IQKeyboardManager.shared.isEnabled = true
-        IQKeyboardManager.shared.enableAutoToolbar = true
-        IQKeyboardManager.shared.resignOnTouchOutside = true
+         setupPushNotifications(application: application)
 
         Analytics.logEvent("app_launched", parameters: nil)
         Crashlytics.crashlytics().log("App started")
@@ -41,8 +31,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    private func configureFirebase() {
-         #if PROD
+     private func configureFirebase() {
+        #if PROD
         let fileName = "GoogleService-Info-Prod"
         #else
         let fileName = "GoogleService-Info-dev"
@@ -55,10 +45,65 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         FirebaseApp.configure(options: options)
+        Messaging.messaging().delegate = self
+
         print("Firebase configured with \(fileName).plist")
     }
 
-    func application(_ application: UIApplication,
+     private func setupNavigationBar() {
+        if #available(iOS 15.0, *) {
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+            navBarAppearance.backgroundColor = .white
+            navBarAppearance.shadowColor = .lightGray
+            navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+
+            UINavigationBar.appearance().standardAppearance = navBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+            UINavigationBar.appearance().compactAppearance = navBarAppearance
+        }
+    }
+
+    private func setupKeyboardManager() {
+        IQKeyboardManager.shared.isEnabled = true
+        IQKeyboardManager.shared.enableAutoToolbar = true
+        IQKeyboardManager.shared.resignOnTouchOutside = true
+    }
+
+     private func setupPushNotifications(application: UIApplication) {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            print("Push permission granted: \(granted)")
+            if let error = error {
+                print("Error requesting push permissions: \(error.localizedDescription)")
+            }
+            if granted {
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            }
+        }
+    }
+
+     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        guard let token = fcmToken else {
+            print(" No FCM token received.")
+            return
+        }
+        #if PROD
+        print("PROD FCM token: \(token)")
+        #else
+        print("DEV FCM token: \(token)")
+        #endif
+    }
+
+     func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .badge, .sound])
+    }
+
+     func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
