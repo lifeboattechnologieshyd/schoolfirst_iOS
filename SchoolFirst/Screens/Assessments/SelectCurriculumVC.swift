e2c22d3e4ef7ddee4c061c11d0324187cd2d106c
ranjith <padidalaranjith@gmail.com>
2025-11-11 19:19:42 +0530

    home works

--- SchoolFirst/Screens/Assessments/SelectCurriculumVC.swift
+++ SchoolFirst/Screens/Assessments/SelectCurriculumVC.swift
@@ -12,7 +12,7 @@ class SelectCurriculumVC: UIViewController {
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var paranparaButton: UIButton!
     @IBOutlet weak var oxfordButton: UIButton!
-    
+    var types = [Curriculum]()
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -24,6 +24,27 @@ class SelectCurriculumVC: UIViewController {
     
     @IBAction func backButtonTapped(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    
+    func getCurriculumType(){
+        NetworkManager.shared.request(urlString: API.CURRICULUM_TYPES, method: .GET) { (result: Result<APIResponse<[Curriculum]>, NetworkError>)  in
+            switch result {
+            case .success(let info):
+                if info.success {
+                    if let data = info.data {
+                        self.types = data
+                    }
+                    DispatchQueue.main.async {
+//                        self.tblVw.reloadData()
+                    }
+                }else{
+                    self.showAlert(msg: info.description)
+                }
+            case .failure(let error):
+                self.showAlert(msg: error.localizedDescription)
+            }
+        }
     }
 }
 
