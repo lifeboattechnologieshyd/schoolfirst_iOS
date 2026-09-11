788bc8ee572c83118911600442d70a2e7981169a
vamshi krishna <vamshikrishna@vamshis-MacBook-Air.local>
2026-09-11 15:51:49 +0530

    created new screen in contactus module

---
+++ SchoolFirst/contactusmodule/ContactslistVC.swift
@@ -0,0 +1,17 @@
+//
+//  ContactslistVC.swift
+//  SchoolFirst
+//
+//  Created by vamshi krishna on 11/09/26.
+//
+
+import UIKit
+class ContactslistVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    }
